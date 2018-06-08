da06868b9dbc04c21952037ac3d8995799491de9
Luke Ledvina <soulgp1@gmail.com>
2018-06-08 11:43:38 -0400

    Start collecting user input

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Menu {
 
-    private Scanner input = new Scanner(System.in)
+    private Scanner input = new Scanner(System.in);
 
     public void mainMenu() {
 
@@ -13,12 +13,39 @@ public class Menu {
                 "\n2. Manage Animals" +
                 "\n3. Manage Items");
 
-        //Handles user input
+        //Handles user input for navigation
+        
+        switch (input.nextInt()) {
+            case 1:
+                //Manage people
+                managePeople();
+                break;
+            case 2:
+                //Manage animals
+                manageAnimal();
+                break;
+            case 3:
+                //Manage inventory
+                manageItems();
+                break;
+            default:
+                //Handle wrong number
+                //Restart method to allow user to try again
+                break;
+        }
 
 
     }
 
+    private void manageItems() {
+    }
 
+    private void manageAnimal() {
+    }
+
+    private void managePeople() {
+        
+    }
 
 
 }
