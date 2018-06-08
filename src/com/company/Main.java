84373b21facd0fb87cee81c990c695076ebac8b9
Luke Ledvina <soulgp1@gmail.com>
2018-06-08 11:01:12 -0400

    Created Menu Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to the zoo");
+
+        Menu menu = new Menu();
+        menu.mainMenu();
 
     }
 }
---
+++ src/com/company/Menu.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    private Scanner input = new Scanner(System.in)
+
+    public void mainMenu() {
+
+        System.out.println("Please choose an option. " +
+               "\n1. Manage People" +
+                "\n2. Manage Animals" +
+                "\n3. Manage Items");
+
+        //Handles user input
+
+
+    }
+
+
+
+
+}
